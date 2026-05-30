c3d20bf4cbd1c3051457ceec224a401906cbb010
Igor Lucas <igorlucas10000@gmail.com>
2026-05-30 18:09:20 +0000

    adicionei o overlay preto de segurança

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,10 @@ import Layout from "../components/Layout.jsx";
 import CountdownGate from "../components/CountdownGate.jsx";
 import "../styles/globals.css";
 
-const UNLOCK = new Date(2026, 5, 2, 0, 0, 0); // 02/06/2026 00:00:00 horário local
+// ✏️ Mude para false para desativar o overlay durante o desenvolvimento
+const GATE_ENABLED = true;
+
+const UNLOCK = new Date(2026, 5, 2, 0, 0, 0); // 02/06/2026 00:00:00
 
 export default function App({ Component, pageProps }) {
   const [gateVisible, setGateVisible] = useState(true);
@@ -15,8 +18,11 @@ export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      {gateVisible && (
-        <CountdownGate unlockDate={UNLOCK} onUnlock={() => setGateVisible(false)} />
+      {GATE_ENABLED && gateVisible && (
+        <CountdownGate
+          unlockDate={UNLOCK}
+          onUnlock={() => setGateVisible(false)}
+        />
       )}
     </Layout>
   );
